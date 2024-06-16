45e2815f2bf7251654e18839729bd6337b193b01
amitB034 <amit.bhan0223@gmail.com>
2024-06-16 23:22:38 +0900

    before

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 
+class Tasks(models.Model):
+    task_title = models.CharField()
+    task_due = models.IntegerField()
+    is_finished = models.BooleanField()
+    
+    def __str__(self):
+        return self.task_title
