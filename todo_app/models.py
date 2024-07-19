fe232bab225cddd802b0ee25efd5de5cf8b4acf2
amitB034 <amit.bhan0223@gmail.com>
2024-07-19 13:37:12 +0900

    アカウント修正

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 class Tasks(models.Model):
 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     task_title = models.CharField(max_length=64)
     task_due = models.DateTimeField(verbose_name='Due Date')
     is_finished = models.BooleanField(default=False)
